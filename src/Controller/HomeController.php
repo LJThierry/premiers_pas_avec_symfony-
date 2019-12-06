e9bfc7fd8975a738cc536281737b6053c7ded2a9
Symfony Codons <codonssymfony@gmail.com>
2019-12-06 17:01:41 +0000

    envoie des fichiers existants

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController extends AbstractController
 {
-    /**
+   
+ /**
      * @Route("/")
      */
-    public function index()
+    
+public function index()
     {
-        return $this->render('home/index.html.twig', [
+        
+return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
